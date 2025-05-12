076e0a55f3750179ffee23bad9f4b6a3ecfe5ba8
KyrillWil <kyrill.willems@student.tsaam.be>
2025-05-12 11:22:56 +0200

    1

--- script/script.js
+++ script/script.js
@@ -1,7 +1,32 @@
+//variabels
 const form = document.getElementById("todo-form");
 const input = document.getElementById("todo-input");
 const list = document.getElementById("todo-list");
+const selectBox = document.createElement("select")
+selectBox.id = "selectBox"
 
+
+//lijst gedaan todos
+const gedaanTodos = [
+  {
+    naam: "appel",
+    winkel: "lidl"
+  },
+  {
+    naam: "banaan",
+    winkel: "carrefour"
+  },
+  {
+    naam: "peer",
+    winkel: "colruyt"
+  },
+  {
+    naam: "kiwwi",
+    winkel: "aldi"
+  }
+]
+
+//
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   const text = input.value.trim();
@@ -15,3 +40,18 @@ form.addEventListener("submit", function (e) {
     input.value = "";
   }
 });
+
+//takenlijst
+list.appendChild(selectBox)
+gedaanTodos.forEach((todo)=>{
+  //vooraf taken in lijst
+  const gedaanTodo = document.createElement("p")
+  gedaanTodo.textContent = todo.naam
+  list.appendChild(gedaanTodo)
+  //dropdown
+  const winkelOptie = document.createElement("option")
+  winkelOptie.textContent = todo.winkel
+  selectBox.appendChild(winkelOptie)
+})
+
+
